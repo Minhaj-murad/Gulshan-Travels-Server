483746b07e2e268282f65ad9b2609a7fc905975f
Minhajmurad <minhajmurad421@gmail.com>
2022-11-09 21:26:49 +0600

    simle server with Mongodb created

--- index.js
+++ index.js
@@ -2,19 +2,42 @@ const express =require('express');
 const app=express();
 const cors =require('cors');
 const port = process.env.PORT || 5000;
-
+const { MongoClient, ServerApiVersion } = require('mongodb');
 app.use(cors());
-
+app.use(express.json());
 app.get('/', (req, res) => {
     res.send('Gulshan Travels API Running');
 });
 
-const hotels=require('./data.json');
 
-app.get('/hotels', (req, res) =>{
-    res.send(hotels);
-});
 
+
+const uri = "mongodb+srv://gulshanTravels:lwUvUABvkcruvA0q@cluster0.owkdqd3.mongodb.net/?retryWrites=true&w=majority";
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run(){
+    try{
+
+    }
+
+finally {
+        
+}
+
+}
+run().catch(error => console.log(error));
+
+
+
+
+
+
+// app.get('/hotels/:id', (req, res) => {
+//     const id =parseInt(req.params.id) ;
+//     const selectedhotelsid = hotels.rooms.find(hotel => hotel.id == id);
+//     console.log(selectedhotelsid);
+//     res.send(selectedhotelsid);
+// });
 app.listen(port, ()=>{
     console.log(`Gulshan Travels is running on port ${port}`);
 })

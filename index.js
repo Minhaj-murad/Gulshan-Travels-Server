8fc1db8f526508ff65f2f8cfb219323947193b06
Minhajmurad <minhajmurad421@gmail.com>
2022-11-09 20:40:38 +0600

    simple server is running

--- index.js
+++ index.js
@@ -8,6 +8,13 @@ app.use(cors());
 app.get('/', (req, res) => {
     res.send('Gulshan Travels API Running');
 });
+
+const hotels=require('./data.json');
+
+app.get('/hotels', (req, res) =>{
+    res.send(hotels);
+});
+
 app.listen(port, ()=>{
     console.log(`Gulshan Travels is running on port ${port}`);
 })

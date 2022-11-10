efe2ef2e3d4b4e91c4a4fd80c735dadaa4992008
Minhajmurad <minhajmurad421@gmail.com>
2022-11-10 18:19:21 +0600

    main requirements done

--- index.js
+++ index.js
@@ -83,6 +83,18 @@ async function run(){
             const result = await hotelcollection.insertOne(hotel);
             res.send(result);
         });
+        app.patch('/hotels/:id', async (req, res) => {
+            const id = req.params.id;
+            const status = req.body.status
+            const query = { _id: ObjectId(id) }
+            const updatedDoc = {
+                $set:{
+                    status: status
+                }
+            }
+            const result = await hotelcollection.updateOne(query, updatedDoc);
+            res.send(result);
+        })
 
 
 
